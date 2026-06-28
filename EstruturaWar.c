b9953def811ddaad745201690ebd9bd58a02f6ff
hagedornpedro-design <hagedornpedro@gmail.com>
2026-06-28 19:40:55 +0000

    Commit do desafio Mestre do jogo War Engenharia de software

--- EstruturaWar.c
+++ EstruturaWar.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-#include <string.h> //biblioteca para rand e alocação dinâmica de memória
+#include <stdlib.h> //biblioteca para rand e alocação dinâmica de memória
 #include <time.h> // biblioteca para o dado aleatório
 
 //Desafio intermediário - Estrutura de dados aula 1 - Jogo War
@@ -16,14 +16,20 @@ typedef struct
     char nome[30]; //armazena o nome com 30 caracteres
     char cor[10]; //armazena a cor com 10 caracteres
     int tropas; //armazena a quantidade de tropas em numeros inteiros
+    char missao[100]; //armazena a missão gerada para o território
+    int tipo;
+    int indiceAlvo;
     } territorio;
 
     
-//criar funções modulares: cadastro, exibição, ataque, liberação
+//criar funções modulares: cadastro, exibição, ataque, liberação, menu, gerar missão e verificar missão
 void cadastro(territorio* mapa, int quantidade);
 void exibirmapa(territorio* mapa, int quantidade);
 void ataque(territorio* atacante, territorio* defensor);
 void liberar(territorio* mapa);
+int exibirMenu();
+int checkmissao(territorio* mapa, int quantidade, territorio* jogadorAtual);
+void gerarmissao(territorio* mapa, int quantidade);
 
 //implementação das funções
 
@@ -107,14 +113,98 @@ void cadastro(territorio* mapa, int quantidade)
             free(mapa);
             printf("Memória liberada com sucesso!\n");
         }
+    //função do menu
+    int exibirMenu(){
+        int opcaoMenu;
+        printf("\n ~~ Menu de opções ~~ \n");
+        printf("Escolha uma opção:\n");
+        printf("1 - Atacar\n");
+        printf("2 - Verificar missão\n");
+        printf("3 - Sair\n");
+        scanf("%d", &opcaoMenu);
+        return opcaoMenu;
+    }
+
+    //função de gerar missão
+        void gerarmissao(territorio* mapa, int quantidade){
+            //inserir a função para gerar a missão
+            //vetor com as missões
+            char* listaMissao[] = {
+                "Conquistar o território %s!",
+                "Eliminar o exército %s!",
+                };
+            //considera cada territorio é um jogador
+            for (int i = 0; i < quantidade; i++){
+                int indiceInimigo; //garante pegar um inimigo e não o proprio jogador
+                do {
+                    indiceInimigo = rand() % quantidade;
+                } while (indiceInimigo == i);
+                //sorteia a missão
+                int tipoMissao = rand() % 2;
+
+                //salvar os indices da missão e do inimigo
+                mapa[i].tipo = tipoMissao;
+                mapa[i].indiceAlvo = indiceInimigo;
+
+                if (tipoMissao == 0) {
+                    sprintf(mapa[i].missao, listaMissao[tipoMissao], mapa[indiceInimigo].nome);
+                } else {
+                    sprintf(mapa[i].missao, listaMissao[tipoMissao], mapa[indiceInimigo].cor);
+                    }
+                }
+            }
+    //função de check missão
+        int checkmissao(territorio* mapa, int quantidade, territorio* jogadorAtual){
+            //recuperar o indice do territorio inimigo
+            int idinimigo = jogadorAtual->indiceAlvo;
+            
+            //implementar a condicional da missão tipo 0
+            if (jogadorAtual->tipo == 0) {
+                //condicional valida se a cor do territorio alvo é da mesma cor que o jogador atual
+                if(strcmp(mapa[idinimigo].cor, jogadorAtual->cor) == 0){
+                    printf("Missão Cumprida!\n");
+                    printf("Vitória do Jogador %s", jogadorAtual->nome);
+                    return 1;
+                }
+            }
+
+            //agora valida se a missão é a eliminação do inimigo
+            else if (jogadorAtual->tipo == 1) {
+                //recupera a cor do exército inimigo recebido na missão
+                char* corInimiga = mapa[idinimigo].cor;
+                int corInimigoExiste = 0;
+
+                //varre o mapa procurando pela cor
+                for (int i = 0; i < quantidade; i++) {
+                    if(strcmp(mapa[i].cor, jogadorAtual->cor) != 0 && strcmp(mapa[i].cor, corInimiga) ==0) {
+                        corInimigoExiste = 1;
+                        break;
+                    }
+                    
+                } if(corInimigoExiste == 0) {
+                    printf("Missão Cumprida!\n");
+                    printf("Vitória do Jogador %s", jogadorAtual->nome);
+                }
+            }
+            printf("Missão atual em andamento\n");
+            return 0;
+        }
+
 
 //função do jogo
 int main()
     {
         int qtde; //declara variável para quantidade de territórios a cadastrar
         int opataque,opdefesa; //declara variável de opção de ataque e defesa
-       printf("Quantos territórios deseja cadastrar?\n");
-       scanf("%d", &qtde);
+        int opcaoMenu = 0;
+       
+       do {
+            printf("Quantos territórios deseja cadastrar?\n");
+            scanf("%d", &qtde);
+            if (qtde < 2) {
+                printf("Cadastre no mínimo dois territórios\n");
+            }
+        } while (qtde <2);
        //fazer alocação de memória necessária para quantidade de territórios para alocar
        territorio* mapa = (territorio*)calloc(qtde, sizeof(territorio));
        
@@ -122,42 +212,70 @@ int main()
 
        exibirmapa(mapa,qtde);
 
+       //aqui entra a geração das missões e definição do turno e jogador, cada territorio é um jogador
+       srand(time(NULL));
+       gerarmissao(mapa,qtde);
+
+       int turno = 0;
+
+
        
-    //loop do jogo até a saída
-    while(1)
+    //loop do jogo incluindo o menu
+    while(opcaoMenu != 3)
         {  
-        //inserir o Ataque
-        printf("\n~~~~~~ FASE DE ATAQUE ~~~~~~ \n");
-        printf("Escolha o território atacante (1 a %d, ou 0 para sair):\n", qtde);
-        scanf("%d", &opataque);
-        if(opataque==0) break; //encerra no zero
-        printf("Escolha o território defensor (1 a %d, ou 0 para sair):\n", qtde);
-        scanf("%d", &opdefesa);
-        if(opdefesa==0) break; //encerra no zero
-        //validação dos dados de entrada
-        if (opataque <= qtde && opdefesa <= qtde)
-            {
-                //chama função de ataque
-                ataque(&mapa[opataque-1], &mapa[opdefesa-1]);
+            //antes de entrar no Menu, identifica o jogador
+            territorio* jogadoratual = &mapa[turno];
+            printf("Sua vez: %s exército %s\n",jogadoratual->nome,jogadoratual->cor);
+            printf("Sua missão é: %s\n",jogadoratual->missao);
+            opcaoMenu = exibirMenu();
 
-                printf("Pressione enter para continuar...\n");
-                while(getchar()!= '\n');
-                getchar();
-                exibirmapa(mapa,qtde);
-
-            }
-            else
-            {
-                printf("Dados inválidos!\n");
-                printf("Pressione enter para continuar...\n");
-                while(getchar()!= '\n');
-                getchar();
-                
-            }
+            switch (opcaoMenu) {
+                case 1: //fase de batalha
+                //inserir o Ataque
+                printf("\n~~~~~~ FASE DE ATAQUE ~~~~~~ \n");
+                printf("Escolha o território atacante (1 a %d, ou 0 para sair):\n", qtde);
+                scanf("%d", &opataque);
+                if(opataque==0) break; //encerra no zero
+                printf("Escolha o território defensor (1 a %d, ou 0 para sair):\n", qtde);
+                scanf("%d", &opdefesa);
+                if(opdefesa==0) break; //encerra no zero
+                //validação dos dados de entrada
+                if (opataque <= qtde && opdefesa <= qtde)
+                    {
+                    //chama função de ataque
+                    ataque(&mapa[opataque-1], &mapa[opdefesa-1]);
+                    turno = (turno + 1)%qtde; //implementa lógica de loop para cada jogador
+                    } else {
+                        printf("Dados inválidos\n");
+                    }
+                    printf("Pressione Enter para continuar...\n");
+                    while(getchar()!= '\n');
+                    getchar();
+                    exibirmapa(mapa,qtde);
+                    break;
+                case 2:
+                    //na verificação da missão, se ela estiver concluída o jogo encerra, caso contrário ele continua
+                    if(checkmissao(mapa, qtde, jogadoratual) == 1){
+                        printf("Fim de jogo!\n");
+                        printf("Vitória do jogador %s\n", jogadoratual->nome);
+                        opcaoMenu = 3;
+                        } else {
+                            //se a missão não finalizou, retorna para o jogo
+                            printf("Pressione Enter para continuar...\n");
+                            while(getchar()!= '\n');
+                            getchar();
+                        }
+                        
+                    break;
+                case 3:
+                    printf("Saindo do jogo...\n");
+                    break;
+                default:
+                    printf("Opção inválida, tente novamente\n");
+                    break;
         }
-        printf("Saindo do jogo, Obrigado por jogar!\n");
-        liberar(mapa);
-        return 0;
 
     }
-    
+    liberar(mapa);
+    return 0;
+}
